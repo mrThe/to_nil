977ea19827bbc81cedc6231e49af607cf2ece80e
Rik Hemsley <rik@rikkus.info>
2015-04-01 15:38:45 +0100

    Added to_nil?

--- spec/to_nil_spec.rb
+++ spec/to_nil_spec.rb
@@ -9,9 +9,21 @@ describe ToNil do
     it "returns nil for a #{klass} instance" do
       expect(klass.new.to_nil).to be_nil
     end
+    
+    it "returns nil for nil.to_nil? for a #{klass} instance" do
+      expect(klass.new.to_nil?).to be_nil
+    end
   end
 
   it "returns nil for nil.to_nil" do
     expect(nil.to_nil).to be_nil
+  end
+  
+  it "returns nil for nil.to_nil? on nil" do
+    expect(nil.to_nil?).to be_nil
+  end
+  
+  it "returns nil for nil.to_nil? on non-nil" do
+    expect(?0.to_nil?).to be_nil
   end
 end
