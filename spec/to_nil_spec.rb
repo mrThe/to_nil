393df4d78ae802920e9e6d1c458c45e03c1cda80
jomo <github@jomo.tv>
2015-04-01 11:18:11 +0200

    fix all the things!

--- spec/to_nil_spec.rb
+++ spec/to_nil_spec.rb
@@ -5,9 +5,13 @@ describe ToNil do
     expect(ToNil::VERSION).not_to be nil
   end
 
-  [String, Object, Hash, Array, nil].each do |klass|
+  [String, Object, Hash, Array].each do |klass|
     it "returns nil for a #{klass} instance" do
       expect(klass.new.to_nil).to be_nil
     end
+  end
+
+  it "returns nil for nil.to_nil" do
+    expect(nil.to_nil).to be_nil
   end
 end
