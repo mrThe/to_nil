c13fb73764d1e3bdaa83ef4daea55ab34ca03f17
Michael Kompanets <mkompanets@gmail.com>
2015-04-02 08:58:45 -0700

    to nil with a bang spec

--- spec/to_nil_spec.rb
+++ spec/to_nil_spec.rb
@@ -9,9 +9,22 @@ describe ToNil do
     it "returns nil for a #{klass} instance" do
       expect(klass.new.to_nil).to be_nil
     end
+
+    it "returns nil for a #{klass} instance with a bang" do
+      expect(klass.new.to_nil!).to be_nil
+    end
+
+    it "raises an error when #{klass} instance is not nil with a bang" do
+      allow_any_instance_of(klass).to receive(:to_nil).and_return('Not Nil!')
+      expect{klass.new.to_nil!}.to raise_error
+    end
   end
 
   it "returns nil for nil.to_nil" do
     expect(nil.to_nil).to be_nil
+  end
+
+  it "returns nil for nil.to_nil with a bang" do
+    expect(nil.to_nil!).to be_nil
   end
 end
